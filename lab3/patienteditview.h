ffc14518e9f65784b4244db53a1cc6993bdff7f0
zjl8881 <3401279035@qq.com>
2026-01-10 11:40:13 +0800

    Add files via upload

--- lab3/patienteditview.h
+++ lab3/patienteditview.h
@@ -12,7 +12,7 @@ class PatientEditView;
 class PatientEditView : public QWidget
 {
     Q_OBJECT
-
+企鹅其额2
 public:
     explicit PatientEditView(QWidget *parent = nullptr, int index = 0);
     ~PatientEditView();
@@ -22,7 +22,6 @@ private slots:
     void on_pushButton_clicked();
     void on_btReport_clicked();
     void handleReportFinished(bool success, QString message);
-1231231231232
 
 private:
     Ui::PatientEditView *ui;
